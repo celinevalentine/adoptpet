78a594f73c93e678dd26667bf895c9f1964a259a
Celine Valentine <cyv419@gmail.com>
2020-08-25 23:17:10 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, redirect, render_template, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
+
+from forms import AddPetForm
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "SECRET!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False
+
+debug = DebugToolbarExtension(app)
+ 
+connect_db(app)
+db.create_all()
+
+@app.route('/')
+def show_index():
+    """show homepage"""
+    return render_template('index.html')
+
+@app.route('/add', methods=["GET", "POST"])
+def add_pet():
+    """submit the form to add a pet"""
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        url = form.url.data
+        age = form.age.data
+        notes = form.notes.data
+        return redirect('/')
+    else:
+        return render_template("new.html", form=form)
+    
+   
---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, BooleanField, IntegerField, RadioField, SelectField
+from wtform.validators import InputRequired, Email, Optional
+
+class AddPetForm(FlaskForm):
+    name = StringField("Pet name", validators=[InputRequired(message="Pet name can't be blank")])
+    species = StringField("Species",validators=[InputRequired(message="Species can't be blank")])
+    photo_url = StringField("Photo URL", validators=[Optional()])
+    age = IntegerField("Age", validators=[Optional()])
+    notes = StringField("Notes", validators=[Optional()])
